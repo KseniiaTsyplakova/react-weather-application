0f10afcc72cbf30e2dee6c1ad231eca3bce02896
Kseniia <tsyplakovakseniia@gmail.com>
2023-01-13 17:29:23 +0400

    Styling changes , added link

--- src/App.js
+++ src/App.js
@@ -9,15 +9,28 @@ export default function App() {
         <Weather />
         <footer>
           This project was coded by{" "}
-          <a href="https://t.me/Kseniia_Tsyplakova" target="_blank">
+          <a
+            href="https://t.me/Kseniia_Tsyplakova"
+            target="_blank"
+            className="text-decoration-none"
+          >
             Kseniia Tsyplakova
           </a>{" "}
           and is{" "}
           <a
             href="https://github.com/KseniiaTsyplakova/react-weather-application"
             target="_blank"
+            className="text-decoration-none"
           >
             open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://teal-mochi-64eb5d.netlify.app/"
+            target="_blank"
+            className="text-decoration-none"
+          >
+            hosted on Netlify
           </a>
         </footer>
       </div>
