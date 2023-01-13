e8ec655dd41374023ca4fca36788b8ac26585473
Kseniia <tsyplakovakseniia@gmail.com>
2023-01-13 11:45:37 +0400

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,20 +3,22 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://t.me/Kseniia_Tsyplakova" target="_blank">
-          Kseniia Tsyplakova
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/KseniiaTsyplakova/react-weather-application"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a href="https://t.me/Kseniia_Tsyplakova" target="_blank">
+            Kseniia Tsyplakova
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/KseniiaTsyplakova/react-weather-application"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
